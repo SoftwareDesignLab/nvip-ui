c62c761712a8e63e163069a532488773f4de98a7
Corey Urbanke <cju8676@rit.edu>
2023-08-16 22:45:13 -0400

    changed VDO editing to multi select every label

--- src/app/models/review-data-criteria.model.ts
+++ src/app/models/review-data-criteria.model.ts
@@ -38,3 +38,43 @@ export interface ReviewDataCriteria {
   vdoUpdates: ReviewVDO
   prodToRemove: Array<number>
 }
+
+export enum VdoNounGroup {
+  IMPACT_METHOD = "Impact Method",
+  CONTEXT = "Context",
+  MITIGATION = "Mitigation",
+  ATTACK_THEATER = "Attack Theater",
+  LOGICAL_IMPACT = "Logical Impact",
+}
+
+export class VdoLabel {
+  // public static readonly TRUST_FAILURE = new VdoLabel(1, "Trust Failure", VdoNounGroup.IMPACT_METHOD)
+  // public static readonly MAN_IN_THE_MIDDLE = new VdoLabel(2, "Man-in-the-Middle", VdoNounGroup.IMPACT_METHOD)
+  // public static readonly CHANNEL = new VdoLabel(3, "Channel", VdoNounGroup.CONTEXT)
+  // public static readonly AUTHENTICATION_BYPASS = new VdoLabel(4, "Authentication Bypass", VdoNounGroup.IMPACT_METHOD)
+  // public static readonly PHYSICAL_HARDWARE = new VdoLabel(5, "Physical Hardware", VdoNounGroup.CONTEXT)
+  // public static readonly APPLICATION = new VdoLabel(6,"Application", VdoNounGroup.CONTEXT)
+  // public static readonly HOST_OS = new VdoLabel(7, "Host OS", VdoNounGroup.CONTEXT)
+  // public static readonly FIRMWARE = new VdoLabel(8, "Firmware", VdoNounGroup.CONTEXT)
+  // public static readonly CODE_EXECUTION = new VdoLabel(9, "Code Execution", VdoNounGroup.IMPACT_METHOD)
+  // public static readonly CONTEXT_ESCAPE = new VdoLabel(10, "Context Escape", VdoNounGroup.IMPACT_METHOD)
+  // public static readonly GUEST_OS = new VdoLabel(11, "Guest OS", VdoNounGroup.CONTEXT)
+  // public static readonly HYPERVISOR = new VdoLabel(12,"Hypervisor", VdoNounGroup.CONTEXT)
+  // public static readonly SANDBOXED = new VdoLabel(13, "Sandboxed", VdoNounGroup.MITIGATION)
+  // public static readonly PHYSICAL_SECURITY = new VdoLabel(14, "Physical Security", VdoNounGroup.MITIGATION)
+  // public static readonly ASLR = new VdoLabel(15, "ASLR", VdoNounGroup.MITIGATION)
+  // public static readonly LIMITED_RMT = new VdoLabel(16, "Limited Rmt", VdoNounGroup.ATTACK_THEATER)
+  // public static readonly LOCAL = new VdoLabel(17, "Local", VdoNounGroup.ATTACK_THEATER)
+  // public static readonly READ = new VdoLabel(18, "Read", VdoNounGroup.LOGICAL_IMPACT)
+  // public static readonly RESOURCE_REMOVAL = new VdoLabel(19, "Resource Removal", VdoNounGroup.LOGICAL_IMPACT)
+  // public static readonly HPKP_HSTS = new VdoLabel(20, "HPKP/HSTS", VdoNounGroup.MITIGATION)
+  // public static readonly MULTIFACTOR_AUTHENTICATION = new VdoLabel(21, "MultiFactor Authentication", VdoNounGroup.MITIGATION)
+  // public static readonly REMOTE = new VdoLabel(22, "Remote", VdoNounGroup.ATTACK_THEATER)
+  // public static readonly WRITE = new VdoLabel(23, "Write", VdoNounGroup.LOGICAL_IMPACT)
+  // public static readonly INDIRECT_DISCLOSURE = new VdoLabel(24, "Indirect Disclosure", VdoNounGroup.LOGICAL_IMPACT)
+  // public static readonly SERVICE_INTERRUPT = new VdoLabel(25, "Service Interrupt", VdoNounGroup.LOGICAL_IMPACT)
+  // public static readonly PRIVILEGE_ESCALATION = new VdoLabel(26, "Privilege Escalation", VdoNounGroup.LOGICAL_IMPACT)
+  // public static readonly PHYSICAL = new VdoLabel(27, "Physical", VdoNounGroup.ATTACK_THEATER);
+
+  constructor(public readonly id: number, public readonly label: string, public readonly group: VdoNounGroup) { }
+}
