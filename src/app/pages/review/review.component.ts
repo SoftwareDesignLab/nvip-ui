da0892014fb6d7911ebcf01685a2a182919e9f2a
Corey Urbanke <cju8676@rit.edu>
2023-08-21 22:05:29 -0400

    formatted/styled vdo tab based on user id

--- src/app/pages/review/review.component.ts
+++ src/app/pages/review/review.component.ts
@@ -30,7 +30,7 @@ import { CookieService } from 'src/app/services/Cookie/cookie.service';
 import { VulnService } from 'src/app/services/vuln/vuln.service';
 import { VDO, Vulnerability } from 'src/app/models/vulnerability.model';
 import { ReviewUpdateCriteria } from 'src/app/models/review-update-criteria.model';
-import { ReviewDataCriteria, ReviewCVSS, ReviewVDO, ReviewVDOLabel } from 'src/app/models/review-data-criteria.model';
+import { ReviewDataCriteria, ReviewVDO, ReviewVDOLabel, VdoLabel, vdoMap } from 'src/app/models/review-data-criteria.model';
 import { ActivatedRoute } from '@angular/router';
 
 export interface updateCvss {
@@ -79,35 +79,8 @@ export class ReviewComponent {
   vdoactive: boolean = false
   aractive: boolean = false
   update = {} as updateObject
-  vdoMap = {
-    TRUST_FAILURE : new VdoLabel(1, "Trust Failure", VdoNounGroup.IMPACT_METHOD),
-    MAN_IN_THE_MIDDLE : new VdoLabel(2, "Man-in-the-Middle", VdoNounGroup.IMPACT_METHOD),
-    CHANNEL : new VdoLabel(3, "Channel", VdoNounGroup.CONTEXT),
-    AUTHENTICATION_BYPASS : new VdoLabel(4, "Authentication Bypass", VdoNounGroup.IMPACT_METHOD),
-    PHYSICAL_HARDWARE : new VdoLabel(5, "Physical Hardware", VdoNounGroup.CONTEXT),
-    APPLICATION : new VdoLabel(6,"Application", VdoNounGroup.CONTEXT),
-    HOST_OS : new VdoLabel(7, "Host OS", VdoNounGroup.CONTEXT),
-    FIRMWARE : new VdoLabel(8, "Firmware", VdoNounGroup.CONTEXT),
-    CODE_EXECUTION : new VdoLabel(9, "Code Execution", VdoNounGroup.IMPACT_METHOD),
-    CONTEXT_ESCAPE : new VdoLabel(10, "Context Escape", VdoNounGroup.IMPACT_METHOD),
-    GUEST_OS : new VdoLabel(11, "Guest OS", VdoNounGroup.CONTEXT),
-    HYPERVISOR : new VdoLabel(12,"Hypervisor", VdoNounGroup.CONTEXT),
-    SANDBOXED : new VdoLabel(13, "Sandboxed", VdoNounGroup.MITIGATION),
-    PHYSICAL_SECURITY : new VdoLabel(14, "Physical Security", VdoNounGroup.MITIGATION),
-    ASLR : new VdoLabel(15, "ASLR", VdoNounGroup.MITIGATION),
-    LIMITED_RMT : new VdoLabel(16, "Limited Rmt", VdoNounGroup.ATTACK_THEATER),
-    LOCAL : new VdoLabel(17, "Local", VdoNounGroup.ATTACK_THEATER),
-    READ : new VdoLabel(18, "Read", VdoNounGroup.LOGICAL_IMPACT),
-    RESOURCE_REMOVAL : new VdoLabel(19, "Resource Removal", VdoNounGroup.LOGICAL_IMPACT),
-    HPKP_HSTS : new VdoLabel(20, "HPKP/HSTS", VdoNounGroup.MITIGATION),
-    MULTIFACTOR_AUTHENTICATION : new VdoLabel(21, "MultiFactor Authentication", VdoNounGroup.MITIGATION),
-    REMOTE : new VdoLabel(22, "Remote", VdoNounGroup.ATTACK_THEATER),
-    WRITE : new VdoLabel(23, "Write", VdoNounGroup.LOGICAL_IMPACT),
-    INDIRECT_DISCLOSURE : new VdoLabel(24, "Indirect Disclosure", VdoNounGroup.LOGICAL_IMPACT),
-    SERVICE_INTERRUPT : new VdoLabel(25, "Service Interrupt", VdoNounGroup.LOGICAL_IMPACT),
-    PRIVILEGE_ESCALATION : new VdoLabel(26, "Privilege Escalation", VdoNounGroup.LOGICAL_IMPACT),
-    PHYSICAL : new VdoLabel(27, "Physical", VdoNounGroup.ATTACK_THEATER),
-  } as VdoMap;
+  vdoMap = vdoMap
+
 
   constructor(
     private vulnService: VulnService,
