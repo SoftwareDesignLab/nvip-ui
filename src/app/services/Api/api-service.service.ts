d295d39b30c73ed4499e9ee6e6117a1c2eb26b85
Corey Urbanke <cju8676@rit.edu>
2023-10-02 16:50:49 -0400

    refactored recent page logic to get by create date
    eliminates need for index and page limit storage, vulns are queried by date

--- src/app/services/Api/api-service.service.ts
+++ src/app/services/Api/api-service.service.ts
@@ -139,13 +139,6 @@ export class ApiService {
       .subscribe(callback);
   }
 
-  vulnServlet(daily: boolean, dateRange: number) {
-    return this.httpClient.get(
-      Routes.vulnerability+"/daily",
-      this.injectGetParameters({ dateRange: dateRange })
-    );
-  }
-
   vulnServGetByID(id: string, username: string, token: string) {
     return this.httpClient.get(
       Routes.vulnerability+'/'+id,
@@ -167,22 +160,11 @@ export class ApiService {
     );
   }
 
-  vulnServGetByDateAndPage(date: string, page: number, size: number) {
+  vulnServGetByDate(date: string) {
     return this.httpClient.get(
-      Routes.vulnerability + '/dailyPage',
+      Routes.vulnerability + '/vulnByDate',
       this.injectGetParameters({
-        date: date,
-        pageNum: page,
-        pageSize: size
-      })
-    );
-  }
-
-  vulnServGetRecentCounts(dateRange: number) {
-    return this.httpClient.get(
-      Routes.vulnerability + '/dailyPageCount',
-      this.injectGetParameters({
-        dateRange: dateRange
+        date: date
       })
     );
   }
